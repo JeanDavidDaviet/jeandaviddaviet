9e3d6ccb3dc5620eeb38ad1e48a00bfd5a5e478f
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2019-06-25 12:38:40 +0200

    Remove useless migrations

--- src/Migrations/Version20190625103802.php
+++ src/Migrations/Version20190625103802.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190622141730 extends AbstractMigration
+final class Version20190625103802 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,10 +22,11 @@ final class Version20190622141730 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('CREATE TABLE post (id INT AUTO_INCREMENT NOT NULL, dossier_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, content LONGTEXT DEFAULT NULL, created_at DATETIME DEFAULT NULL, updated_at DATETIME DEFAULT NULL, excerpt VARCHAR(500) DEFAULT NULL, published TINYINT(1) DEFAULT \'0\' NOT NULL, INDEX IDX_5A8A6C8D611C0C56 (dossier_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE dossier (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE post ADD dossier_id INT DEFAULT NULL');
+        $this->addSql('CREATE TABLE portfolio (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, link VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE page (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, published TINYINT(1) DEFAULT \'0\' NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8D611C0C56 FOREIGN KEY (dossier_id) REFERENCES dossier (id)');
-        $this->addSql('CREATE INDEX IDX_5A8A6C8D611C0C56 ON post (dossier_id)');
     }
 
     public function down(Schema $schema) : void
@@ -34,8 +35,9 @@ final class Version20190622141730 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_5A8A6C8D611C0C56');
+        $this->addSql('DROP TABLE post');
         $this->addSql('DROP TABLE dossier');
-        $this->addSql('DROP INDEX IDX_5A8A6C8D611C0C56 ON post');
-        $this->addSql('ALTER TABLE post DROP dossier_id');
+        $this->addSql('DROP TABLE portfolio');
+        $this->addSql('DROP TABLE page');
     }
 }
