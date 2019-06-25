661a1f2ba42e908f8542bd611e596b74cc11631c
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2019-06-25 13:04:27 +0200

    Remove portfolio for now

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -8,36 +8,30 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PortfolioController extends AbstractController
 {
-    /**
-     * @Route("/portfolio", name="portfolio")
-     */
-    public function index()
-    {
-        $portfolios = $this->getDoctrine()->getRepository(Portfolio::class)->findAll();
+    // public function index()
+    // {
+    //     $portfolios = $this->getDoctrine()->getRepository(Portfolio::class)->findAll();
 
-        if(!$portfolios){
-            throw $this->createNotFoundException('Aucun projet trouvé');
-        }
+    //     if(!$portfolios){
+    //         throw $this->createNotFoundException('Aucun projet trouvé');
+    //     }
 
-        return $this->render('portfolio/portfolio.html.twig', [
-            'portfolios' => $portfolios,
-        ]);
-    }
-    /**
-     * @Route("/portfolio/{slug}", name="projet")
-     */
-    public function projet($slug)
-    {
-        $projet = $this->getDoctrine()->getRepository(Portfolio::class)->findOneBy([
-            'slug' => $slug
-        ]);
+    //     return $this->render('portfolio/portfolio.html.twig', [
+    //         'portfolios' => $portfolios,
+    //     ]);
+    // }
+    // public function projet($slug)
+    // {
+    //     $projet = $this->getDoctrine()->getRepository(Portfolio::class)->findOneBy([
+    //         'slug' => $slug
+    //     ]);
 
-        if(!$projet){
-            throw $this->createNotFoundException('Aucun projet trouvé');
-        }
+    //     if(!$projet){
+    //         throw $this->createNotFoundException('Aucun projet trouvé');
+    //     }
 
-        return $this->render('portfolio/projet.html.twig', [
-            'projet' => $projet,
-        ]);
-    }
+    //     return $this->render('portfolio/projet.html.twig', [
+    //         'projet' => $projet,
+    //     ]);
+    // }
 }
