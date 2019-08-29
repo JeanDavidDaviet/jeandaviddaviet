40a46f678014bde41bbbd1beef5f389a787fe370
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2019-08-29 19:14:59 +0200

    refactor category listing and fix archive posts_per_page

--- archive.php
+++ archive.php
@@ -1,1 +1,8 @@
-<?php $template = locate_template('home.php'); load_template($template); ?>
+<?php get_header(); ?>
+
+<section class="wrapper--narrow">
+
+  <?php get_template_part('components/listing'); ?>
+
+</section>
+<?php get_footer(); ?>
