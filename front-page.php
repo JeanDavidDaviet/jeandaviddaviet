724218fa8dae0c0671dd4f63950fcf194b0d6161
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2020-02-28 09:14:31 +0100

    try some last articles on frontpage

--- front-page.php
+++ front-page.php
@@ -8,4 +8,18 @@
     </blockquote>
   </div>
 
+  <div class="homepage-last">
+    <h3>Les derniers articles</h3>
+    <p>Qu’est-ce que la spécificité CSS ?</p>
+    <p>Lire un fichier .db (SQLite) et afficher son contenu</p>
+    <p>Ajouter TypeScript à un project ReactJS existant</p>
+    <p><a href="#">Plus d'articles</a></p>
+
+    <h3>Les derniers dossiers</h3>
+    <p>Aujourdhui j'ai appris</p>
+    <p>Comprendre les sites internet</p>
+    <p>CSS</p>
+    <p><a href="#">Plus de dossiers</a></p>
+  </div>
+
 <?php get_footer(); ?>
