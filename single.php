f4b65c88452fe732aa7e1bef0f5b823dc407c6b0
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2020-07-10 13:38:31 +0200

    small fix of table styling and missing space in between categories link

--- single.php
+++ single.php
@@ -4,7 +4,7 @@
 
       <article itemtype="https://schema.org/Article">
         <div class="article__title"><h1 itemprop="name"><?php the_title(); ?></h1></div>
-        <time class="article__meta" datetime="<?php echo get_the_date('Y-m-d'); ?>" itemprop="datePublished"><small><?php _e("Publié le", "jdd"); ?> <?php echo get_the_date('d/m/Y'); ?> <?php _e("par", "jdd"); ?> <?php echo get_the_author(); ?> <?php _e("dans", "jdd"); ?> <?php echo get_the_category_list(',') ?></small></time>
+        <time class="article__meta" datetime="<?php echo get_the_date('Y-m-d'); ?>" itemprop="datePublished"><small><?php _e("Publié le", "jdd"); ?> <?php echo get_the_date('d/m/Y'); ?> <?php _e("par", "jdd"); ?> <?php echo get_the_author(); ?> <?php _e("dans", "jdd"); ?> <?php echo get_the_category_list(', '); ?></small></time>
         <div class="article__content"><?php the_content(); ?></div>
       </article>
 
