83f9b5a366ce71d02c61c0920bcf6df03c243022
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2021-06-26 09:18:15 +0200

    add manually working webshare

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,16 @@
 import "./components/testimonies";
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(function(reg) {
+    // registration worked
+    // console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch(function(error) {
+    // registration failed
+    // console.log('Registration failed with ' + error);
+  });
+};
+
+document.querySelector('.switch-theme')?.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  document.cookie = `theme=${document.body.classList.contains('dark') ? 'dark' : 'light'}`;
+});
