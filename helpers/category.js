c28fd25c5952df243c727e639cd20cff16bc60db
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2022-03-05 13:07:27 +0100

    add category listing

--- helpers/category.js
+++ helpers/category.js
@@ -9,3 +9,13 @@ export const getCategoryById = async (id) => {
     const category = await res.json()
     return category;
 }
+
+export const getCategoryBySlug = async (slug) => {
+    const res = await fetch('https://jeandaviddaviet.fr/wp-json/wp/v2/categories')
+    const categories = await res.json();
+    const category = categories.filter(category => category.slug === slug);
+    console.log(slug, category);
+    return category.length ? category[0] : null;
+}
+
+}
