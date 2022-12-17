4cdd46294bb03e9c225825ad50b4d4be2bed1678
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2022-12-17 15:40:38 +0100

    use serviceworker from root instead

--- src/js/index.ts
+++ src/js/index.ts
@@ -6,7 +6,7 @@ document.querySelector('.switch-theme')?.addEventListener('click', () => {
 });
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register(urls.theme + '/sw.js', { scope: '/' }).then(function(reg) {
+  navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(function(reg) {
     console.log('Registration succeeded. Scope is ' + reg.scope);
   }).catch(function(error) {
     console.log('Registration failed with ' + error);
