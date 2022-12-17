124525b95987ccb8d0ec8987be9a0642fe95eb7d
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2022-12-17 16:06:42 +0100

    remove service worker script for PHP HTTP redirect

--- src/js/index.ts
+++ src/js/index.ts
@@ -4,11 +4,3 @@ document.querySelector('.switch-theme')?.addEventListener('click', () => {
   document.body.classList.toggle('dark');
   document.cookie = `theme=${document.body.classList.contains('dark') ? 'dark' : 'light'}`;
 });
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(function(reg) {
-    console.log('Registration succeeded. Scope is ' + reg.scope);
-  }).catch(function(error) {
-    console.log('Registration failed with ' + error);
-  });
-};
