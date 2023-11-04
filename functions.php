ca93d7887ef3b41ea8ef4aa66319c0b72e138e54
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2023-11-04 15:10:49 +0100

    fix warning using PHP 8.1

--- functions.php
+++ functions.php
@@ -20,9 +20,12 @@ require_once 'inc/cpt.php';
 
 function wp_remove_vers( $src ) {
   global $wp_version;
-  parse_str(parse_url($src, PHP_URL_QUERY), $query);
-  if ( !empty($query['ver']) && $query['ver'] === $wp_version ) {
-    $src = remove_query_arg('ver', $src);
+  $version = parse_url($src, PHP_URL_QUERY);
+  if ($version) {
+    parse_str(parse_url($src, PHP_URL_QUERY), $query);
+    if ( !empty($query['ver']) && $query['ver'] === $wp_version ) {
+      $src = remove_query_arg('ver', $src);
+    }
   }
   return $src;
 }
